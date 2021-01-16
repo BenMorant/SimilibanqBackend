c8bd3452f67d101534e27fd555f6671d8ca7e508
Benjamin Morant <bmorant@oxiane.com>
2021-01-16 15:54:07 +0100

    Initial commit

---
+++ src/main/java/com/benmorant/katas/bankingapp/similibanq/SimilibanqApplication.java
@@ -0,0 +1,13 @@
+package com.benmorant.katas.bankingapp.similibanq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimilibanqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimilibanqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benmorant/katas/bankingapp/similibanq/SimilibanqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benmorant.katas.bankingapp.similibanq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimilibanqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
