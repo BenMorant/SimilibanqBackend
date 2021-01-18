9dd1f79712518a8e66301b74a916a4f37a30a209
Ben <52816705+benmorant@users.noreply.github.com>
2021-01-18 22:27:08 +0100

    T26-removing once usefull, now useless classes and methods

--- src/main/java/com/benmorant/katas/bankingapp/similibanq/dao/DepositDao.java
+++
@@ -1,6 +0,0 @@
-package com.benmorant.katas.bankingapp.similibanq.dao;
-
-import com.benmorant.katas.bankingapp.similibanq.entity.Deposit;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepositDao extends JpaRepository<Deposit, Long> {}
--- src/main/java/com/benmorant/katas/bankingapp/similibanq/dao/SavingsAccountDao.java
+++
@@ -1,6 +0,0 @@
-package com.benmorant.katas.bankingapp.similibanq.dao;
-
-import com.benmorant.katas.bankingapp.similibanq.entity.SavingsAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SavingsAccountDao extends JpaRepository<SavingsAccount, Long> {}
